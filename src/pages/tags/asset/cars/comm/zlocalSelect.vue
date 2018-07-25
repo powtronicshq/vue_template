d205cbe224843bab2f9f8033a9acf8f3c6d4b7c4
haiquan.shi <haiquan.shi@zhaoguanche.com>
2018-07-25 18:09:12 +0800

    提交

---
+++ src/pages/tags/asset/cars/comm/zlocalSelect.vue
@@ -0,0 +1,43 @@
+<style>
+.el-select .el-input__inner {
+  width: 100%;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+.z-input{
+  
+}
+</style>
+
+<template>
+  <div class='z-input'>
+    <el-select v-model="selectValue" :style='{"width":width}' :placeholder="placeholder">
+      <el-option v-for="item in options" :key="item" :label="item" :value="item">
+      </el-option>
+    </el-select>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["width", "placeholder", "value", "options"],
+  model: {
+    prop: "value",
+    event: "changeValue"
+  },
+  data() {
+    return {
+      selectValue: this.value
+    };
+  },
+  watch: {
+    selectValue(val) {
+      this.$emit("changeValue", val);
+    },
+    value(val) {
+      this.selectValue = val;
+    }
+  }
+};
+</script>
+
--- src/pages/tags/asset/cars/index.vue
+++ src/pages/tags/asset/cars/index.vue
@@ -1,37 +1,181 @@
-<template>
-  <div class="hello">
-     <el-input v-model="input" placeholder="资产管理-车辆管理">sssssssssssssss</el-input>
-     <el-button type="text" @click="push">dd</el-button>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.wrapper{
+  max-width: 1000px;
+  width: 100%;
+}
+</style>
 
+<template>
+  <div class="wrapper">
+    <el-form style='display:flex;margin-top:0px;width:100%' label-width="80px">
+      <el-form-item label="状态" label-width="40px" style='font-size:16px;font-weight:bold'>
+        <localSelect width='100px' v-model="statusDefault"  :options='statusOptions' ></localSelect>
+      </el-form-item>
+      <el-form-item label="入库仓库" label-width="70px" style='font-size:16px;font-weight:bold;margin-left:20px'>
+        <remoteSelect v-model="stoneName" :reqPromise='reqStoneNamePromise' width='200px' placeholder='请输入仓库名称'></remoteSelect>
+      </el-form-item>
+      <el-form-item label="计划创建时间" label-width="100px" style='font-size:16px;font-weight:bold;margin-left:20px'>
+        <el-date-picker style='width:250px' v-model="createDate" type="daterange" range-separator="-" start-placeholder="开始日期" end-placeholder="结束日期">
+        </el-date-picker>
+      </el-form-item>
+
+      <el-form-item label="入库类型" label-width="70px" style='font-size:16px;font-weight:bold;margin-left:20px'>
+        <localSelect width='140px' v-model="typeDefault"  :options='typeOptions'></localSelect>
+      </el-form-item>
+
+      <el-form-item label="车牌号" label-width="70px" style='font-size:16px;font-weight:bold;margin-left:20px'>
+        <remoteSelect  v-model="plate" :reqPromise='reqPlatePromise' width='150px'></remoteSelect>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script>
-
+import remoteSelect from "./comm/zremoteSelect";
+import localSelect from "./comm/zlocalSelect";
 
 export default {
-  name: '',
-     props: ['addTags','closeLocalTag',"param"],
+  name: "",
+  props: ["addTags", "closeLocalTag", "param"],
 
-  data () {
+  data() {
     return {
-      input:''
-    }
+      statusOptions:["全部","待入库","已入库","已取消"],
+      statusDefault:'全部',
+      stoneName:'',
+      createDate:'',
+      typeOptions:["全部采购入库","调拨入库","回收入库"],
+      typeDefault:'全部采购入库',
+      plate:'沪dada',
+
+    };
   },
-  mounted(){
-     console.dir("this.param")
-     console.dir(this.param)
+  mounted() {
+   
   },
-  methods:{
-     push(){
-     this.closeLocalTag()
-    }
+  components: {
+    remoteSelect,
+    localSelect
+  },
+  methods: {
+    reqStoneNamePromise(query) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve([
+            'Alabamasssssssssssssssssssssssszzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzsssssssssss',
+            'Alaska',
+            'Arizona',
+            'Arkansas',
+            'California',
+            'Colorado',
+            'Connecticut',
+            'Delaware',
+            'Florida',
+            'Georgia',
+            'Hawaii',
+            'Idaho',
+            'Illinois',
+            'Indiana',
+            'Iowa',
+            'Kansas',
+            'Kentucky',
+            'Louisiana',
+            'Maine',
+            'Maryland',
+            'Massachusetts',
+            'Michigan',
+            'Minnesota',
+            'Mississippi',
+            'Missouri',
+            'Montana',
+            'Nebraska',
+            'Nevada',
+            'New Hampshire',
+            'New Jersey',
+            'New Mexico',
+            'New York',
+            'North Carolina',
+            'North Dakota',
+            'Ohio',
+            'Oklahoma',
+            'Oregon',
+            'Pennsylvania',
+            'Rhode Island',
+            'South Carolina',
+            'South Dakota',
+            'Tennessee',
+            'Texas',
+            'Utah',
+            'Vermont',
+            'Virginia',
+            'Washington',
+            'West Virginia',
+            'Wisconsin',
+            'Wyoming'
+          ])
+        }, 5000)
+      })
+    },
+    reqPlatePromise(query) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve([
+            'Alabamasssssssssssssssssssssssszzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzsssssssssss',
+            'Alaska',
+            'Arizona',
+            'Arkansas',
+            'California',
+            'Colorado',
+            'Connecticut',
+            'Delaware',
+            'Florida',
+            'Georgia',
+            'Hawaii',
+            'Idaho',
+            'Illinois',
+            'Indiana',
+            'Iowa',
+            'Kansas',
+            'Kentucky',
+            'Louisiana',
+            'Maine',
+            'Maryland',
+            'Massachusetts',
+            'Michigan',
+            'Minnesota',
+            'Mississippi',
+            'Missouri',
+            'Montana',
+            'Nebraska',
+            'Nevada',
+            'New Hampshire',
+            'New Jersey',
+            'New Mexico',
+            'New York',
+            'North Carolina',
+            'North Dakota',
+            'Ohio',
+            'Oklahoma',
+            'Oregon',
+            'Pennsylvania',
+            'Rhode Island',
+            'South Carolina',
+            'South Dakota',
+            'Tennessee',
+            'Texas',
+            'Utah',
+            'Vermont',
+            'Virginia',
+            'Washington',
+            'West Virginia',
+            'Wisconsin',
+            'Wyoming'
+          ])
+        }, 5000)
+      })
+    },
   }
-}
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-
-
-</style>
